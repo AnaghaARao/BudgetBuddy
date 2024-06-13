2efc436cb38bc5aa5f8651e3eab41f3f0f734a60
AnaghaARao <rao.anagha2002@gmail.com>
2024-06-13 19:58:34 +0530

    api call completed

--- BudgetBuddy/static/js/register.js
+++ BudgetBuddy/static/js/register.js
@@ -1,22 +1,21 @@
 const usernameField = document.querySelector("#usernameField");
+const feedBackField = document.querySelector(".invalid-feedback");
 
 usernameField.addEventListener("keyup", (e) => {
     console.log('77777',77777);
 
     const usernameVal = e.target.value;
-
     // console.log('usernameVal', usernameVal); -> testing
-
-
     if(usernameVal.length > 0){
         fetch("/authentication/validate-username",{
-            body:{username: usernameVal},
+            body:JSON.stringify({username: usernameVal }),
             method: "POST",
         }).then(res=>res.json())
         .then(data=>{
             console.log("data", data);
+            if(data.username_error){
+                usernameField.classList.add("is-invalid");
+            }
         });
-    }
-
-    
+    } 
 });

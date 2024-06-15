746f1ec852d4f45ba95cae469f4cac47d32a1963
AnaghaARao <rao.anagha2002@gmail.com>
2024-06-15 13:04:10 +0530

    username validation complete

--- BudgetBuddy/static/js/register.js
+++ BudgetBuddy/static/js/register.js
@@ -1,10 +1,15 @@
 const usernameField = document.querySelector("#usernameField");
-const feedBackField = document.querySelector(".invalid-feedback");
+const feedBackArea = document.querySelector(".invalid-feedback");
 
 usernameField.addEventListener("keyup", (e) => {
     console.log('77777',77777);
 
     const usernameVal = e.target.value;
+
+    // to prevent the error message display initially
+    usernameField.classList.remove("is-invalid");
+    feedBackArea.style.display='none'; // to display incase username-error
+
     // console.log('usernameVal', usernameVal); -> testing
     if(usernameVal.length > 0){
         fetch("/authentication/validate-username",{
@@ -15,6 +20,8 @@ usernameField.addEventListener("keyup", (e) => {
             console.log("data", data);
             if(data.username_error){
                 usernameField.classList.add("is-invalid");
+                feedBackArea.style.display='block'; // to display incase username-error
+                feedBackArea.innerHTML = `<p>${data.username_error}<p>`
             }
         });
     } 

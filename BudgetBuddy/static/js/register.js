7a71fa81b2c2bbb512fe98d9457cfb32f5f6a111
AnaghaARao <rao.anagha2002@gmail.com>
2024-06-17 17:01:58 +0530

    email validation and username checking ui done

--- BudgetBuddy/static/js/register.js
+++ BudgetBuddy/static/js/register.js
@@ -2,13 +2,19 @@ const usernameField = document.querySelector("#usernameField");
 const feedBackArea = document.querySelector(".invalid_feedback");
 const emailField = document.querySelector("#emailFieldxx");
 const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
 
 usernameField.addEventListener("keyup", (e) => {
     console.log('77777',77777); // test
     const usernameVal = e.target.value;
+
+    usernameSuccessOutput.style.display = "block";
+    usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
+
     // to prevent the error message display initially
     usernameField.classList.remove("is-invalid");
     feedBackArea.style.display='none'; // to display incase username-error
+
     if(usernameVal.length > 0){
         fetch("/authentication/validate-username",{
             body:JSON.stringify({username: usernameVal }),
@@ -16,6 +22,7 @@ usernameField.addEventListener("keyup", (e) => {
         }).then(res=>res.json())
         .then(data=>{
             console.log("data", data);
+            usernameSuccessOutput.style.display = "none";
             if(data.username_error){
                 usernameField.classList.add("is-invalid");
                 feedBackArea.style.display='block'; // to display incase username-error
@@ -27,7 +34,7 @@ usernameField.addEventListener("keyup", (e) => {
 
 emailField.addEventListener("keyup", (e) => {
     console.log("88888", 88888);
-
+    
     emailVal = e.target.value;
     emailField.classList.remove("is-invalid");
     emailFeedBackArea.style.display = "none";

8c9616775ba1b8de58cdb96930dfdfa2ab2f81b1
AnaghaARao <rao.anagha2002@gmail.com>
2024-06-25 07:52:43 +0530

    ajax based search implementation complete

--- expenses/views.py
+++ expenses/views.py
@@ -5,7 +5,21 @@ from .models import Category, Expense
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 # Create your views here.
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
 
 @login_required(login_url='/authentication/login')
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)

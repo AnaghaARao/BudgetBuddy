a525f78868598ae6763f3c808685c90d54f6855a
AnaghaARao <rao.anagha2002@gmail.com>
2024-06-25 08:37:02 +0530

    search fixes

--- expenses/views.py
+++ expenses/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 import json
 from django.http import JsonResponse
+from userpreferences.models import UserPreference
+# from django.contrib.auth.models import UserPreference
 # Create your views here.
 
 def search_expenses(request):
@@ -29,9 +31,12 @@ def index(request):
     paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+
     context = {
         'expenses': expenses,
         'page_obj': page_obj,
+        'cuurency': currency,
     }
     return render(request, 'expenses/index.html', context)
 
